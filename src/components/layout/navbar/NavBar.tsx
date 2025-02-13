ed8508f8f4585747c71d077f17275a87d8c73c11
Itumeleng Seema <itumelengseema@outlook.com>
2025-02-13 13:49:39 +0200

    Add utility functions, components, and Tailwind configuration for navbar and UI elements

---
+++ src/components/layout/navbar/NavBar.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "../navbar/logo";
+import { NavMenu } from "./nav-menu";
+import { NavigationSheet } from "@/components/layout/navbar/navigation-sheet";
+
+const Navbar = () => {
+  return (
+    <div className="min-h-screen bg-muted">
+      <nav className="h-16 bg-background border-b">
+        <div className="h-full flex items-center justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+          <Logo />
+
+          {/* Desktop Menu */}
+          <NavMenu className="hidden md:block" />
+
+          <div className="flex items-center gap-3">
+            <Button variant="outline" className="hidden sm:inline-flex">
+              Sign In
+            </Button>
+            <Button>Get Started</Button>
+
+            {/* Mobile Menu */}
+            <div className="md:hidden">
+              <NavigationSheet />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/layout/navbar/logo.tsx
@@ -0,0 +1,8 @@
+import logo from "../../../../public/branding/logo sebara.svg";
+import Image from 'next/image';
+export const Logo = () => (
+    <Image src={logo} alt="Sebara Logo" width="124"
+    height="32"  />
+    
+  );
+  

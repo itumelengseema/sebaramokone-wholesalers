cae68e262ebfba56900b44f599d99b688172b199
Itumeleng Seema <itumelengseema@outlook.com>
2025-06-24 22:09:49 +0200

    feat: add initial category schema definition with name field

---
+++ src/sanity/category.ts
@@ -0,0 +1,11 @@
+const category =  {
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    { name: 'name', title: 'Name', type: 'string' }
+  ]
+};
+export default category;
+
+

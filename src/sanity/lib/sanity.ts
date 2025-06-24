b949b01b0355e66aeeb3242826ff1376420bd9ca
Itumeleng Seema <itumelengseema@outlook.com>
2025-06-24 22:40:34 +0200

    fix: update projectId in Sanity client configuration

--- src/sanity/lib/sanity.ts
+++ src/sanity/lib/sanity.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export const sanityClient = createClient({
-  projectId: 'your_project_id',
+  projectId: 'bojmv7k2',
   dataset: 'production',
   apiVersion: '2024-06-01',
   useCdn: true,
